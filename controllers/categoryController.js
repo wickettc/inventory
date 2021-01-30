b880f2a4b91a83078329d73fbed5bf9f5224c3e1
Chase Wickett <wickettc@yahoo.com>
2021-01-30 00:13:28 -0500

    still working on 404 error

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -39,51 +39,62 @@ exports.category_create_get = function (req, res) {
     res.render('category_form', { title: 'Create Category' });
 };
 
-exports.category_create_post = [
-    body('name', 'Category name required')
-        .trim()
-        .isLength({ min: 1 })
-        .escape()
-        .withMessage('Name must be specified'),
-    body('description', 'Category description required')
-        .trim()
-        .isLength({ min: 1 })
-        .escape()
-        .withMessage('Description must be specified'),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        const category = new Category({
-            name: req.body.name,
-            description: req.body.description,
-        });
-        if (!errors.isEmpty()) {
-            //errors are present
-            res.render('category_form', {
-                title: 'Create Category',
-                category: category,
-                errors: errors.array(),
-            });
-            return;
-        } else {
-            //data is valid
-            Category.findOne({ name: req.body.name }).exec(function (
-                err,
-                found_category
-            ) {
-                if (err) return next(err);
-                if (found_category) {
-                    res.redirect(found_category.url);
-                } else {
-                    category.save(function (err) {
-                        if (err) return next(err);
-                        //save new category and redirect to page
-                        res.redirect(category.url);
-                    });
-                }
-            });
-        }
-    },
-];
+// exports.category_create_post = [
+//     body('name', 'Category name required')
+//         .trim()
+//         .isLength({ min: 1 })
+//         .escape()
+//         .withMessage('Name must be specified'),
+//     body('description', 'Category description required')
+//         .trim()
+//         .isLength({ min: 1 })
+//         .escape()
+//         .withMessage('Description must be specified'),
+//     (req, res, next) => {
+//         const errors = validationResult(req);
+//         const category = new Category({
+//             name: req.body.name,
+//             description: req.body.description,
+//         });
+//         if (!errors.isEmpty()) {
+//             //errors are present
+//             res.render('category_form', {
+//                 title: 'Create Category',
+//                 category: category,
+//                 errors: errors.array(),
+//             });
+//             return;
+//         } else {
+//             //data is valid
+//             Category.findOne({ name: req.body.name }).exec(function (
+//                 err,
+//                 found_category
+//             ) {
+//                 if (err) return next(err);
+//                 if (found_category) {
+//                     res.redirect(found_category.url);
+//                 } else {
+//                     category.save(function (err) {
+//                         if (err) return next(err);
+//                         //save new category and redirect to page
+//                         res.redirect(category.url);
+//                     });
+//                 }
+//             });
+//         }
+//     },
+// ];
+
+exports.category_create_post = function (req, res, next) {
+    const category = new Category({
+        name: req.body.name,
+        description: req.body.description,
+    });
+    category.save(function (err) {
+        if (err) return next(err);
+        res.redirect(category.url);
+    });
+};
 
 exports.category_delete_get = function (req, res) {
     res.send('Category delete GET');

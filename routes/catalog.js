a4475f42b59433bc598a82a9a85b14bfe512b8d3
Chase Wickett <wickettc@yahoo.com>
2021-01-30 11:22:38 -0500

    updated routes to fix 404 error

--- routes/catalog.js
+++ routes/catalog.js
@@ -9,25 +9,25 @@ const category_controller = require('../controllers/categoryController');
 router.get('/', item_controller.index);
 
 router.get('/item/create', item_controller.item_create_get);
-router.get('/item/create', item_controller.item_create_post);
+router.post('/item/create', item_controller.item_create_post);
 
 router.get('/item/:id/delete', item_controller.item_delete_get);
-router.get('/item/:id/delete', item_controller.item_delete_post);
+router.post('/item/:id/delete', item_controller.item_delete_post);
 
 router.get('/item/:id/update', item_controller.item_update_get);
-router.get('/item/:id/update', item_controller.item_update_post);
+router.post('/item/:id/update', item_controller.item_update_post);
 
 router.get('/items', item_controller.item_list);
 router.get('/item/:id', item_controller.item_detail);
 
 router.get('/category/create', category_controller.category_create_get);
-router.get('/category/create', category_controller.category_create_post);
+router.post('/category/create', category_controller.category_create_post);
 
 router.get('/category/:id/delete', category_controller.category_delete_get);
-router.get('/category/:id/delete', category_controller.category_delete_post);
+router.post('/category/:id/delete', category_controller.category_delete_post);
 
 router.get('/category/:id/update', category_controller.category_update_get);
-router.get('/category/:id/update', category_controller.category_update_post);
+router.post('/category/:id/update', category_controller.category_update_post);
 
 router.get('/categories', category_controller.category_list);
 router.get('/category/:id', category_controller.category_detail);
